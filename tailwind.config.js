c3f1149e70bd935eef35f7ca832dfac1bc2479cd
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-03-17 19:33:58 +0200

    feat(styles): Add default styles for fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    fontSize: {
+      sm: '11px',
+      base: '14px',
+      xl: '16px',
+      '2xl': '20px',
+      '3xl': '24px',
+      '4xl': '32px'
+    }
   },
   plugins: []
 }
