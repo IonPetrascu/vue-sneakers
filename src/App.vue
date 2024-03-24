d8d49ce7de0bf43a43bd83b2dda47721343b1e53
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-03-24 15:07:12 +0200

    feat:get sneakers from server and add filtration and search sneakers

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,76 @@
 <script setup>
+import { onMounted,ref, watch,reactive } from 'vue';
+import axios from 'axios'
+
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
-</script>
+
+const items = ref([])
+
+
+
+const filters = reactive({
+  sortBy:'title',
+  searchQuery:''
+})
+const onChangeSelect = (e)=>{
+  filters.sortBy = e.target.value
+}
+const onChangeSearchInput = (e)=>{
+  filters.searchQuery = e.target.value
+}
+
+
+const fecthItems = async()=>{
+try {
+  const params = {
+    sortBy:filters.sortBy,
+
+  }
+  if(filters.searchQuery){
+    params.title =  `*${filters.searchQuery}*`
+  }
+
+    const {data} = await axios.get('https://b71d9efcf989be11.mokky.dev/items',{
+      params
+    })
+    items.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+onMounted(fecthItems)
+watch(filters,fecthItems)
+
+
+ </script>
 
 <template>
   <div className="mt-14 w-4/5 max-w-7xl m-auto rounded-3xl shadow-xl bg-white">
     <Header />
     <div className="p-10">
-      <h2 className="text-3xl font-bold mb-5">Все кроссовоки</h2>
-      <CardList/>
+      <div class="flex justify-between items-center">
+        <h2 className="text-3xl font-bold mb-5">Все кроссовоки</h2>
+        <div class="flex gap-4">
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none" name="" id="">
+            <option value="title">По названию</option>
+            <option value="price">По цене(дешевые)</option>
+            <option value="-price">По цене(дорогие)</option>
+          </select>
+            <div class="relative">
+              <img class="absolute left-3 top-3" src="/search.svg" alt="">
+              <input @input="onChangeSearchInput" class="border border-gray-200 focus:border-gray-400 rounded-md py-2 pl-10 pr-4 outline-none" placeholder="Поиск...">
+            </div>
+        </div>
+      </div>
+      <div class="mt-10">
+
+        <CardList :items="items"/>
+      </div>
     </div>
   </div>
-  <Drawer/>
+ <!--  <Drawer/> -->
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,17 +1,21 @@
 <script setup>
 import Cart from './Cart.vue';
+
+  defineProps({
+  items:Array
+  })
+
 const onClickAdd = () =>{
   alert('add')
 }
 const onClickFavorite = () =>{
   alert('favorite')
 }
+
 </script>
 
 <template>
 <div className="grid grid-cols-4 gap-5">
-      <Cart :onClickAdd="onClickAdd" :onClickFavorite="onClickFavorite" :isFavorite="true" :isAdded="true" :price="5000" title="Мужские Кроссовки Nike Blazer Mid Suede" imageUrl="./sneakers/sneakers-1.jpg"/>
-
-
+      <Cart v-for="item in items" :key="item.id" :onClickAdd="onClickAdd" :onClickFavorite="onClickFavorite" :id="item.id" :isFavorite="true" :isAdded="true" :price="item.price" :title="item.title" :imageUrl="item.imageUrl"/>
     </div>
 </template>
