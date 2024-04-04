38b20884b86e0b43b0ecc57977bcc47f410b49ea
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-04-04 15:54:50 +0300

    refactor: Rename file

---
+++ src/components/DrawerHead.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { inject } from 'vue';
+
+  const {closeDrawer} = inject('cart')
+</script>
+
+<template>
+<div class="flex justify-between items-center pb-4 flex-row-reverse">
+        <button @click="closeDrawer" class="flex items-center text-2xl font-bold">
+           Закрыть
+          <svg class="" width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 7H14.7143" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M8.71436 1L14.7144 7L8.71436 13" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+
+        </button>
+        <h2 class="text-2xl font-bold">Корзина</h2>
+      </div>
+</template>
