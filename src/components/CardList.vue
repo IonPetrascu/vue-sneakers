338c6b32ad846e507f73cb6fad9db10a1b474b25
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-04-06 10:19:19 +0300

    feat: Add animaton to drawerList and cardList

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -11,7 +11,7 @@ const emit = defineEmits(['addToFavorite','onClickAddPlus'])
 </script>
 
 <template>
-<div className="grid grid-cols-4 gap-5">
+<div v-auto-animate className="grid grid-cols-4 gap-5">
       <Card :onClickAdd="()=> emit('onClickAddPlus',item)" :onClickFavorite="() => emit('addToFavorite', item)" v-for="item in items" :is-added="item.isAdded" :key="item.id"  :id="item.id" :isFavorite="item.isFavorite"  :price="item.price" :title="item.title" :imageUrl="item.imageUrl"/>
     </div>
 </template>
--- src/components/CartItemList.vue
+++ src/components/CartItemList.vue
@@ -10,7 +10,7 @@ const {cart,removeFromCart} = inject('cart')
 
 
 <template>
-   <div class="pt-8 overflow-y-auto h-[713px]" >
+   <div v-auto-animate class="pt-8 overflow-y-auto h-[713px]" >
       <CartItem :key="item.id" v-for="item in cart" :title="item.title" @remove-from-cart="()=> removeFromCart(item)" :imageUrl="item.imageUrl" :price="item.price"/>
       </div>
 </template>
